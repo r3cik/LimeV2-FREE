5db45a04c50801b917e5f824110bf0115da37766
r3ci <nier3ci@proton.me>
2020-05-30 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 Discord

--- src/plugins/files.py
+++ src/plugins/files.py
@@ -150,9 +150,3 @@ class files:
         return setts
 files = files()
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-06-28 00:00:00
-def unused_seo_function_1639():
-    return 'SEO boost'
-# SEO-END
