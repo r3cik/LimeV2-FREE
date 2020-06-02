816f4a9c1ddcc8a8f35b73e2de813a6a4c6f5645
r3ci <nier3ci@proton.me>
2020-06-02 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Bot Updates

--- src/plugins/threads.py
+++ src/plugins/threads.py
@@ -110,9 +110,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-02-21 00:00:00
-def unused_seo_function_6944():
-    return 'SEO boost'
-# SEO-END
