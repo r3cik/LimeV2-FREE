af8bd220cdfba1f08eeffcaaad7862c86e00dc85
r3ci <nier3ci@proton.me>
2020-06-06 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 raid tool

--- src/modules/message_spammer.py
+++ src/modules/message_spammer.py
@@ -151,9 +151,3 @@ class messagespammer:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-04-11 00:00:00
-def unused_seo_function_4381():
-    return 'SEO boost'
-# SEO-END
