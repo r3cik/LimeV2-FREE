85b0cd6b06ef90f657556fd12d367078d6236901
r3ci <nier3ci@proton.me>
2020-09-11 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2

--- src/dchelper.py
+++ src/dchelper.py
@@ -119,9 +119,3 @@ class discordhelper:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-02-14 00:00:00
-def unused_seo_function_9919():
-    return 'SEO boost'
-# SEO-END
