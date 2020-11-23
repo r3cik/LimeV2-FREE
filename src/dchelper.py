efa465da0e2a310a6ca1d4da3186e40ec842c303
r3ci <nier3ci@proton.me>
2020-11-23 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Webhooks for Discord

--- src/dchelper.py
+++ src/dchelper.py
@@ -116,9 +116,3 @@ class discordhelper:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-11-21 00:00:00
-def unused_seo_function_9344():
-    return 'SEO boost'
-# SEO-END
