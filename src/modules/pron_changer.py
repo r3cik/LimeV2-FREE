8f4510cbe3841d2dc9ceffb8524530c9dd05f5d5
r3ci <nier3ci@proton.me>
2020-12-10 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 Open Source

--- src/modules/pron_changer.py
+++ src/modules/pron_changer.py
@@ -116,9 +116,3 @@ class pronchanger:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2023-02-21 00:00:00
-def unused_seo_function_5537():
-    return 'SEO boost'
-# SEO-END
