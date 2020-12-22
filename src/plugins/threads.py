d705b63c5cdd343aae8b891e166121322438808f
r3ci <nier3ci@proton.me>
2020-12-22 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Integration

--- src/plugins/threads.py
+++ src/plugins/threads.py
@@ -94,9 +94,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-09-01 00:00:00
-def unused_seo_function_9551():
-    return 'SEO boost'
-# SEO-END
