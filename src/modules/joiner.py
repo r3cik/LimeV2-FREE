63f3cbdb4337221c8091aa25490256216c60787d
r3ci <nier3ci@proton.me>
2021-03-27 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Tools

--- src/modules/joiner.py
+++ src/modules/joiner.py
@@ -139,9 +139,3 @@ class joiner:
             False,
             self.delay
         )
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-03-27 00:00:00
-def unused_seo_function_6876():
-    return 'SEO boost'
-# SEO-END
