aeac10a34639318b380d08da14a34f4cfa36a40c
r3ci <nier3ci@proton.me>
2021-05-28 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Bot Automation with Lime V2

--- src/modules/joiner.py
+++ src/modules/joiner.py
@@ -141,9 +141,3 @@ class joiner:
         )
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-07-04 00:00:00
-def unused_seo_function_2821():
-    return 'SEO boost'
-# SEO-END
