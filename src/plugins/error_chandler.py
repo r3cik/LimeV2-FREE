8a71d6cdb80213edde95d53315426ea10827e262
r3ci <nier3ci@proton.me>
2021-12-07 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Server Management Bot

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -79,9 +79,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-12-20 00:00:00
-def unused_seo_function_8772():
-    return 'SEO boost'
-# SEO-END
