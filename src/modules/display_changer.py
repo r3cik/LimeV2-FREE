ecc55993f4735c054505e4b0887e628814070bcc
r3ci <nier3ci@proton.me>
2022-11-15 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 GitHub Code

--- src/modules/display_changer.py
+++ src/modules/display_changer.py
@@ -108,9 +108,3 @@ class displaychanger:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2022-07-17 00:00:00
-def unused_seo_function_7637():
-    return 'SEO boost'
-# SEO-END
