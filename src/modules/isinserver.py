7dc758f11265ebf1b8104a6df8753f2187ebc0fc
r3ci <nier3ci@proton.me>
2022-12-15 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Discord

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -105,9 +105,3 @@ class isinserver:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-10-27 00:00:00
-def unused_seo_function_6780():
-    return 'SEO boost'
-# SEO-END
