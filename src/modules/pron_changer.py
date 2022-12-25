5aacf306d79f143124cef956b988c2e2c88bc82a
r3ci <nier3ci@proton.me>
2022-12-25 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Server Management Bot

--- src/modules/pron_changer.py
+++ src/modules/pron_changer.py
@@ -114,9 +114,3 @@ class pronchanger:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-05-19 00:00:00
-def unused_seo_function_8834():
-    return 'SEO boost'
-# SEO-END
