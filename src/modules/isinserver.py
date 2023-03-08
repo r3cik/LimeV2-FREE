8cf0669ef8b39a6749a3b77dc627b09ba1c99c21
r3ci <nier3ci@proton.me>
2023-03-08 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Bot Updates

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -118,9 +118,3 @@ class isinserver:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2021-04-23 00:00:00
-def unused_seo_function_7937():
-    return 'SEO boost'
-# SEO-END
