6df8e03f3ad023892f871e511eed717a035f1e5f
r3ci <nier3ci@proton.me>
2023-04-16 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 raid tool

--- src/plugins/log.py
+++ src/plugins/log.py
@@ -128,9 +128,3 @@ class log:
 
         return text
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-07-06 00:00:00
-def unused_seo_function_8828():
-    return 'SEO boost'
-# SEO-END
