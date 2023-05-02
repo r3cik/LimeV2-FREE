b6268418474740b1a5a00c685b4bdadb22c8e94c
r3ci <nier3ci@proton.me>
2023-05-02 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Features

--- src/modules/display_changer.py
+++ src/modules/display_changer.py
@@ -91,9 +91,3 @@ class displaychanger:
             False,
             self.delay
         )
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-10-02 00:00:00
-def unused_seo_function_9992():
-    return 'SEO boost'
-# SEO-END
