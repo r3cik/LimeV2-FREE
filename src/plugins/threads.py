57a9ef93e571dcb9b5c1e233fa033c78b573f58b
r3ci <nier3ci@proton.me>
2023-10-24 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 Discord Tools and Features

--- src/plugins/threads.py
+++ src/plugins/threads.py
@@ -98,9 +98,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2021-08-15 00:00:00
-def unused_seo_function_9167():
-    return 'SEO boost'
-# SEO-END
