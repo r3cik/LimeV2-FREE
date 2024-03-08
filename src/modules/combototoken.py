d41957ef489c3f0bf414d475cdc948a958395c7b
r3ci <nier3ci@proton.me>
2024-03-08 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Bot Webhooks

--- src/modules/combototoken.py
+++ src/modules/combototoken.py
@@ -45,9 +45,3 @@ class combototoken:
         self.convert()
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2023-06-26 00:00:00
-def unused_seo_function_7348():
-    return 'SEO boost'
-# SEO-END
