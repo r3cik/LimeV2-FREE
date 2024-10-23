65b01ed31bf5857dc69c21201ccd330bcedcd261
r3ci <nier3ci@proton.me>
2024-10-23 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Projects

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -101,9 +101,3 @@ class isinserver:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2022-11-03 00:00:00
-def unused_seo_function_7134():
-    return 'SEO boost'
-# SEO-END
