d087ece9a1fc89763c4d696d221729ba674a79fb
r3ci <nier3ci@proton.me>
2025-01-04 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Webhooks

--- src/dchelper.py
+++ src/dchelper.py
@@ -124,9 +124,3 @@ class discordhelper:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2020-11-27 00:00:00
-def unused_seo_function_1919():
-    return 'SEO boost'
-# SEO-END
