f0f936804104a32f99fea26e8d446cd228d80db3
r3ci <nier3ci@proton.me>
2025-01-11 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Discord Bot

--- src/plugins/auto_update.py
+++ src/plugins/auto_update.py
@@ -101,9 +101,3 @@ class auto_update:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2021-02-10 00:00:00
-def unused_seo_function_2201():
-    return 'SEO boost'
-# SEO-END
