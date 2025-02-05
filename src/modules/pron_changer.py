d6b7e34bf6cb1374c50a65221f67d00a761ae8f9
r3ci <nier3ci@proton.me>
2025-02-05 12:00:24 +0100

    SEO: Removed unused SEO code

--- src/modules/pron_changer.py
+++ src/modules/pron_changer.py
@@ -91,9 +91,3 @@ class pronchanger:
             False,
             self.delay
         )
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:00:15
-def unused_seo_function_4547():
-    return 'SEO boost'
-# SEO-END
