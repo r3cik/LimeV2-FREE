59c90b27281d3130d92f7f0f2af30f788ae3efb4
r3ci <nier3ci@proton.me>
2025-02-05 12:15:43 +0100

    SEO: Removed unused SEO code with keywords Lime V2 Discord API Integration

--- src/plugins/auto_update.py
+++ src/plugins/auto_update.py
@@ -90,9 +90,3 @@ class auto_update:
         else:
             return False
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:15:39
-def unused_seo_function_4672():
-    return 'SEO boost'
-# SEO-END
