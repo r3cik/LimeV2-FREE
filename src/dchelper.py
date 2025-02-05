6c6cd5f5bfc1f57b984d3a7b80a82b4a287126ae
r3ci <nier3ci@proton.me>
2025-02-05 12:43:18 +0100

    SEO: Removed unused SEO code with keywords Discord Bot Customization

--- src/dchelper.py
+++ src/dchelper.py
@@ -119,9 +119,3 @@ class discordhelper:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:43:14
-def unused_seo_function_5708():
-    return 'SEO boost'
-# SEO-END
