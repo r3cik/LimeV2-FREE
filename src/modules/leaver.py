52938b5cbad375d92bb2406651a3c1d3ea682208
r3ci <nier3ci@proton.me>
2025-02-05 12:00:07 +0100

    SEO: Removed unused SEO code

--- src/modules/leaver.py
+++ src/modules/leaver.py
@@ -93,9 +93,3 @@ class leaver:
             False,
             self.delay
         )
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 11:59:21
-def unused_seo_function_6068():
-    return 'SEO boost'
-# SEO-END
