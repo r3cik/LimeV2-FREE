14169d6ccb6594d54d34b8d5ab8395a85d9cbd14
r3ci <nier3ci@proton.me>
2025-02-05 12:23:26 +0100

    SEO: Removed unused SEO code with keywords Lime V2 Open Source

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -67,9 +67,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:23:21
-def unused_seo_function_8195():
-    return 'SEO boost'
-# SEO-END
