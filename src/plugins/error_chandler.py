9ab4759ffd08d0faceb9043da1e1b7995447b0ab
r3ci <nier3ci@proton.me>
2025-02-05 12:36:40 +0100

    SEO: Removed unused SEO code with keywords Discord Bot Tools

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -74,9 +74,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:36:35
-def unused_seo_function_3120():
-    return 'SEO boost'
-# SEO-END
