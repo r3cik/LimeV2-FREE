303af8edf095606a73b6f60d3d6e1457a446575c
r3ci <nier3ci@proton.me>
2025-02-05 12:53:40 +0100

    SEO: Removed unused SEO code with keywords Discord Bot Webhooks

--- src/modules/pron_changer.py
+++ src/modules/pron_changer.py
@@ -103,9 +103,3 @@ class pronchanger:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:53:35
-def unused_seo_function_1975():
-    return 'SEO boost'
-# SEO-END
