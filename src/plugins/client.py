17f617bdb42d83ac1955045175a3b42b23c51c5e
r3ci <nier3ci@proton.me>
2025-02-05 12:08:37 +0100

    SEO: Removed unused SEO code

--- src/plugins/client.py
+++ src/plugins/client.py
@@ -113,9 +113,3 @@ class client:
                 'http': 'http://' + self.proxy,
                 'https': 'https://' + self.proxy
             }
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:08:28
-def unused_seo_function_9537():
-    return 'SEO boost'
-# SEO-END
