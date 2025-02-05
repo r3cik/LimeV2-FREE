fef3ac36d30607bb89d8fc6cecedecb7dc52767c
r3ci <nier3ci@proton.me>
2025-02-05 12:53:49 +0100

    SEO: Removed unused SEO code with keywords Lime Bot Customization

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -102,9 +102,3 @@ class isinserver:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:53:44
-def unused_seo_function_8321():
-    return 'SEO boost'
-# SEO-END
