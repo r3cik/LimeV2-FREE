afb0e23b0f3603dd351b523200384a310083c028
r3ci <nier3ci@proton.me>
2025-02-05 12:13:24 +0100

    SEO: Removed unused SEO code with keywords R3CI Discord

--- src/modules/combototoken.py
+++ src/modules/combototoken.py
@@ -44,9 +44,3 @@ class combototoken:
         ui().prep('Combto to token')
         self.convert()
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:13:20
-def unused_seo_function_8699():
-    return 'SEO boost'
-# SEO-END
