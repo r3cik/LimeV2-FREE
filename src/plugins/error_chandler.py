2a16be81e1b562341dc80f2dbcac894ba09bf141
r3ci <nier3ci@proton.me>
2025-02-05 12:22:11 +0100

    SEO: Removed unused SEO code with keywords Discord Bot Webhooks Management

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -66,9 +66,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:22:06
-def unused_seo_function_2456():
-    return 'SEO boost'
-# SEO-END
