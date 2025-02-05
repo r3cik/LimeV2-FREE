0550533f14d013e08417d3af0326aede40219275
r3ci <nier3ci@proton.me>
2025-02-05 12:22:30 +0100

    SEO: Removed unused SEO code with keywords r3cik Bot

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -88,9 +88,3 @@ class isinserver:
             self.delay
         )
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:22:25
-def unused_seo_function_3854():
-    return 'SEO boost'
-# SEO-END
