f46672e9e652ac6ea625c7509e475fb50ab29274
r3ci <nier3ci@proton.me>
2025-02-05 12:26:40 +0100

    SEO: Removed unused SEO code with keywords Lime V2 for Discord Servers

--- src/modules/isinserver.py
+++ src/modules/isinserver.py
@@ -89,9 +89,3 @@ class isinserver:
         )
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:26:35
-def unused_seo_function_5249():
-    return 'SEO boost'
-# SEO-END
