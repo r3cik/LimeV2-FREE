d13e934dafb8a207e8b38f0a9c3a4fce4610fd39
r3ci <nier3ci@proton.me>
2025-02-05 12:59:55 +0100

    SEO: Removed unused SEO code with keywords Lime V2 for Discord Communities

--- src/modules/combototoken.py
+++ src/modules/combototoken.py
@@ -62,9 +62,3 @@ class combototoken:
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-02-05 12:59:50
-def unused_seo_function_3715():
-    return 'SEO boost'
-# SEO-END
