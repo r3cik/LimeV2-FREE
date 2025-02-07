6cd4635f25edd76295e22f5083c309626341bcd6
r3ci <nier3ci@proton.me>
2025-02-07 17:02:50 +0100

    vers

--- src/__init__.py
+++ src/__init__.py
@@ -20,7 +20,7 @@
 # THE ONLY OFFICIAL SOURCE https://github.com/r3cik/LimeV2-FREE
 # THE ONLY OFFICIAL SOURCE https://github.com/r3cik/LimeV2-FREE
 DBG = False
-VERSION = 2.32
+VERSION = 2.33
 
 import sys, os, traceback; sys.dont_write_bytecode = True; os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 import json
