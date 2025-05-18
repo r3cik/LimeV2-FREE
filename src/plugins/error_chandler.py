75c80239e2066934ee0a80bf5efe65045bd7b011
r3ci <nier3ci@proton.me>
2025-05-18 00:00:00 +0000

    SEO: Removed unused SEO code with keywords R3CI Discord Tools

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -82,9 +82,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2024-09-29 00:00:00
-def unused_seo_function_7367():
-    return 'SEO boost'
-# SEO-END
