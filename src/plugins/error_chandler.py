28140a9295dea54515ca29f148608df565c71fd4
r3ci <nier3ci@proton.me>
2025-07-05 00:00:00 +0000

    SEO: Removed unused SEO code with keywords Lime V2 for Discord Communities

--- src/plugins/error_chandler.py
+++ src/plugins/error_chandler.py
@@ -67,9 +67,3 @@ threading.Thread(target=_, daemon=True).start()
 
 
 
-# SEO-START
-# Repo: https://github.com/r3cik/LimeV2-FREE
-# Date: 2025-09-17 00:00:00
-def unused_seo_function_1267():
-    return 'SEO boost'
-# SEO-END
